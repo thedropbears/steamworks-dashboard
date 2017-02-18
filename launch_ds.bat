f697f0d90d4291fba4f7c56d09a3d7d06ec72ede
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-02-18 17:17:02 +1100

    Set resolution of window

--- launch_ds.bat
+++ launch_ds.bat
@@ -1,11 +1,8 @@
 @echo off
 
 
-REM The page order is so random...
+
 start "" "C:\Program Files (x86)\FRC Driver Station\DriverStation"
-start http://localhost:8888/ --window-size=1366,570 --window-position=0,0
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://localhost:8888 --window-size=1400,580 --window-position=0,0
 
-
-cd %~dp0
-py -3 server.py --robot 10.47.74.2
-pause
+py -3 server.py --robot=10.47.74.2
