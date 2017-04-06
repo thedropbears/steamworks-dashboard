b6a50cb994b837379c51d8472459e8cee1e2e2a8
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-04-07 00:46:09 +1000

    Changed stream locations and bat file

--- launch_ds.bat
+++ launch_ds.bat
@@ -5,4 +5,5 @@
 start "" "C:\Program Files (x86)\FRC Driver Station\DriverStation"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://localhost:8888 --window-size=1400,580 --window-position=0,0
 
-py -3 server.py --robot=10.47.74.2
+start "" py -3 ../pyntlogger/logger.py
+py -3 server.py --robot=10.47.74.2 
