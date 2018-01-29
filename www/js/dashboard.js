d1a7336114a71f0792bfe4b4fcb55e475728ecac
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2018-01-29 22:35:14 +1100

    Change timer value

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -212,7 +212,7 @@ function rotateCompass(heading) {
 }
 
 function timerCycle() {
-    var countDownDate = Math.floor(Date.now() / 1000) + 005;
+    var countDownDate = Math.floor(Date.now() / 1000) + 135;
     var x = setInterval(function () {
         var now = Math.floor(Date.now() / 1000);
         var difference = countDownDate - now;
