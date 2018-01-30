bab8803ca3e45c7b5e3378e3edec759870f3894d
Joel Bryla <joel@wiredlizard.com>
2018-01-30 16:11:21 +1100

    Converted to new networktables2js and updated files

--- launch_ds.bat
+++ launch_ds.bat
@@ -5,5 +5,4 @@
 start "" "C:\Program Files (x86)\FRC Driver Station\DriverStation"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://localhost:8888 --window-size=1400,580 --window-position=0,0
 
-start "" py -3 ../pyntlogger/logger.py
-py -3 server.py --robot=10.47.74.2 
+py -3 -m pynetworktables2js --robot=roborio-4774-frc.local
