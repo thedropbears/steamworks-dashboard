ed8e91ba8b7bd93322b85117e8d9134eba4c7bb5
Joel Bryla <joel@wiredlizard.com>
2018-01-30 17:59:35 +1100

    added auto hiding checklist and removed 2017 specific functions

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,20 +1,12 @@
-var targetRange = 4; // TODO work out how far we can see
-var camera = 1;
-var loop = 1;
 var currentGyro = 0
 var offsetGyro = 0
 var cameraStream1 = "http://rpi3-4774.local:1181/"
-var cameraStream2 = "http://10.47.74.11:5802/?action=stream"
-var reverse = false;
 var alliance = "red"
-var currentState = "stationary"
 var timerStart = false;
 var firstReset = false;
 
 $(document).ready(function () {
-
     $("#camera1").attr("src", cameraStream1);
-    $('#camera2').attr("src", cameraStream2)
     $("#state").attr("src", "img/icons/stationaryred.png");
     $("#compass").attr("src", "img/robotred.png");
     $("#robotSVG").attr("xlink:href", "img/robotred.png");
@@ -32,33 +24,14 @@ $(document).ready(function () {
 
     // hook up our SendableChoosers to combo boxes
     attachSelectToSendableChooser("#auto-select", "/SmartDashboard/Autonomous Mode");
+
+    $("#checklist").submit(remove_form)
 });
-//buttons
-/*
-function switchCamera(value) {
-    if (value === "front") {
-        $("#camera2").css("display", "none");
 
-        $("#camera1").css("display", "inline");
-        camera = 1
-
-    } else if (value === "back") {
-        $("#camera1").css("display", "none");
-
-        $("#camera2").css("display", "inline")
-        camera = 2
-    }
-
-}
-*/
 function resetVideo() {
 
     if (camera === 1) {
         $("#camera").removeAttr("src").attr("src", cameraStream1);
-
-    } else if (camera === 2) {
-        $("#camera").removeAttr("src").attr("src", cameraStream2);
-
 
     }
 }
@@ -93,33 +66,28 @@ function onRobotConnection(connected) {
 
     }
 }
+function remove_form() {
+    $(".checklist-div").hide()
+    $(".inital-hide").show()
+}
 
 function onValueChanged(key, value, isNew) {
     switch (key) {
         case "/robot/mode":
-            if (value === "teleop"){
-            if (!timerStart) {
-                timerCycle();
-            }}
-            /*if (value != "disabled"){
-                if (!started){
-                resetGyro();
-                started = true;
-            }}*/
+            if (value === "teleop") {
+                if (!timerStart) {
+                    timerCycle();
+                }
+            }
+            if (value != "disabled"){
+                remove_form()
+            }
 
             break;
 
         case "/SmartDashboard/gyro":
             rotateCompass(value + Math.PI);
             currentGyro = value;
-            break;
-
-        case "/SmartDashboard/rail_pos":
-            var railPos = value + 1;
-            // XXX wtf does this do
-            railPos = railPos * 50;
-            railPos = railPos * 0.9;
-            document.getElementById("railRect").setAttribute("x", railPos + "%");
             break;
 
 
@@ -133,71 +101,13 @@ function onValueChanged(key, value, isNew) {
             }
             $("#compass").attr("src", "img/robot" + alliance + ".png");
             $("#robotSVG").attr("src", "img/robot" + alliance + ".png");
-            $("#state").attr("src", "img/icons/" + currentState + alliance + ".png")
             $("#robotSVG").attr("xlink:href", "img/robot" + alliance + ".png");
 
-            break;
-
-        case "/SmartDashboard/state":
-            currentState = value;
-            $("#state").attr("src", "img/icons/" + value + alliance + ".png");
-            break;
-
-        case "/components/vision/x":
-            changeRobotStrafePos(value);
-
-            var railVisionpos = value + 1;
-            // XXX wtf does this do
-            railVisionpos = railVisionpos * 50;
-            railVisionpos = railVisionpos * 0.9;
-            document.getElementById("railVision").setAttribute("x", railVisionpos + "%");
-            if (-0.1 <= value && value <= 0.1){
-                document.getElementById("railVision").setAttribute("fill", "green");
-            }
-            else{
-                document.getElementById("railVision").setAttribute("fill", "yellow");                
-            }
-           break;
-
-        case "/SmartDashboard/range":
-            if (currentState === "unloadingGear") {
-            changeRobotRange(value)
-            }
-            break;
-        
-        case "/SmartDashboard/camera":
-            switchCamera(value)
             break;
 
         case "/SmartDashboard/reset_video":
             resetVideo()
             break;
-        
-    }
-}
-
-function changeRobotRange(dist) {
-    var ypos = (dist - targetRange) / targetRange;
-    if (ypos >= 1.0) {
-        ypos = 1.0;
-    }
-    ypos += 1; // value from -1 to 1
-    ypos = ypos * 50;
-    ypos = (ypos / 100) * 15
-    ypos = ypos + "em";
-    document.getElementById("robotSVG").setAttribute("y", ypos);
-
-}
-
-function changeRobotStrafePos(visionX) {
-    visionX = -visionX;
-    if (visionX >= -1.0 && visionX <= 1.0) {
-        var robot = document.getElementById("robotSVG");
-
-        var xpos = visionX + 1; // -1 -to 1 now 0 to 2
-        xpos = xpos * 50; // percentage 
-        xpos = xpos * 0.65; // width of picture  
-        document.getElementById("robotSVG").setAttribute("x", xpos + "%");
 
     }
 }
@@ -217,18 +127,18 @@ function timerCycle() {
         var now = Math.floor(Date.now() / 1000);
         var difference = countDownDate - now;
 
-		if (difference <= 0) {
+        if (difference <= 0) {
             document.getElementById("cycleTimer").innerHTML = "";
-			$("#timerInfo").text("GOOD JOB!");
-			$("#timerInfo").toggleClass("blink");
-		}
-		else if (difference < 10) {
+            $("#timerInfo").text("GOOD JOB!");
+            $("#timerInfo").toggleClass("blink");
+        }
+        else if (difference < 10) {
             document.getElementById("cycleTimer").innerHTML = "00" + difference;
         } else if (difference < 100) {
             document.getElementById("cycleTimer").innerHTML = "0" + difference;
         }
-		else {
+        else {
             document.getElementById("cycleTimer").innerHTML = difference;
-		}
+        }
     }, 1000);
 }
