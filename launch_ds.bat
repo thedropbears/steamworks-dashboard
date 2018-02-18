f3c04377b3aefac37c7b69538b51e38e92abb8ba
Joel Bryla <joel@wiredlizard.com>
2018-02-18 14:05:08 +1100

    added team in launch.bat

--- launch_ds.bat
+++ launch_ds.bat
@@ -5,4 +5,4 @@
 start "" "C:\Program Files (x86)\FRC Driver Station\DriverStation"
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://localhost:8888 --window-size=1400,580 --window-position=0,0
 
-py -3 -m pynetworktables2js --robot=roborio-4774-frc.local
+py -3 -m pynetworktables2js --team 4774
