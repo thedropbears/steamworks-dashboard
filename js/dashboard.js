9839ac010b77a977fe7c2ab057d5b34d80de7917
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2018-02-18 12:16:08 +1100

    Remove checklist on click

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,8 @@
 var currentGyro = 0
 var offsetGyro = 0
-var cameraStream1 = "http://rpi3-4774.local:1181/stream.mjpg"
 var alliance = "red"
 var other_alliance = "blue"
 var timerStart = false;
-var firstReset = false;
 var timerFrom = 135;
 var timerCounter = true;
 var intervalTimer;
@@ -22,6 +20,8 @@ $(document).ready(function () {
     // hook up our SendableChoosers to combo boxes
     attachSelectToSendableChooser("#auto-select", "/SmartDashboard/Autonomous Mode");
 
+    $("input").click(removeForm);
+
     loadCameraOnConnect(
     {container: '#camera1',
     port: 1181,
@@ -31,23 +31,17 @@ $(document).ready(function () {
     attrs: {
         width: 640,
         height: 480
-    }
-});
-
-    $("#checklist").submit(remove_form)
-});
+    },},);
+}, );
 
 function onValueChanged(key, value, isNew) {
     switch (key) {
         case "/robot/mode":
-            if (value === "teleop") {
-                if (!timerStart) {
+            if (value === "teleop" && !timerStart) {
                     startTimer();
-                    remove_form();
+                    removeForm();
                     break;
-                }
-            }
-            if (value === "disabled") {
+            } else if (value === "disabled") {
                 resetTimer();
                 break;
             }
@@ -60,7 +54,8 @@ function onValueChanged(key, value, isNew) {
             break;
 
         case "/FMSInfo/GameSpecificMessage":
-            set_map_locations(value)
+            setMapLocations(value)
+            break;
 
         case "/FMSInfo/IsRedAlliance":
             if (value === true) {
@@ -75,14 +70,12 @@ function onValueChanged(key, value, isNew) {
             $("#compass").attr("src", "img/robot" + alliance + ".png");
             $("#robotSVG").attr("src", "img/robot" + alliance + ".png");
             $("#robotSVG").attr("xlink:href", "img/robot" + alliance + ".png");
-
             break;
-
     }
 }
 
 
-function set_map_locations(locations) {
+function setMapLocations(locations) {
     enemy_switch = locations[0] + "1"
     scale = locations[1] + "2"
     our_switch = locations[2] + "3"
@@ -91,12 +84,12 @@ function set_map_locations(locations) {
     $("#"+scale).addClass(alliance).removeClass(other_alliance)
     $("#"+our_switch).addClass(alliance).removeClass(other_alliance)
 
-    $("#"+side_switch(enemy_switch)).addClass(other_alliance).removeClass(alliance)
-    $("#"+side_switch(scale)).addClass(other_alliance).removeClass(alliance)
-    $("#"+side_switch(our_switch)).addClass(other_alliance).removeClass(alliance)
+    $("#"+sideSwitch(enemy_switch)).addClass(other_alliance).removeClass(alliance)
+    $("#"+sideSwitch(scale)).addClass(other_alliance).removeClass(alliance)
+    $("#"+sideSwitch(our_switch)).addClass(other_alliance).removeClass(alliance)
 }
 
-function side_switch(a){
+function sideSwitch(a){
     if (a[0] === "L"){
         return "R"+a[1]
     }
@@ -105,17 +98,17 @@ function side_switch(a){
     }
 }
 
-function remove_form() {
-    $(".checklist-div").hide()
-    $(".inital-hide").show()
+function removeForm() {
+    if ($("input").length === $("input:checked").length){
+    	$(".checklist-div").hide()
+    	$(".inital-hide").show()
+    }
 }
 
 function rotateCompass(heading) {
     heading = heading - offsetGyro;
     heading = Math.PI - heading; // gyro is the wrong way around
-    var robot = document.getElementById("compass");
-    robot.style.transform = "rotate(" + heading + "rad)";
-
+    document.getElementById("compass").style.transform = "rotate(" + heading + "rad)";
 }
 
 function startTimer() {
@@ -128,24 +121,23 @@ function resetTimer() {2
     clearInterval(intervalTimer);
     timerFrom = 135;
     intervalTimer = null;
-    document.getElementById("cycleTimer").innerHTML = "135";
+    $("#cycleTimer").text(135);
 }
 
 function timer() {
     if (timerCounter) {
         timerFrom = timerFrom - 1
         if (timerFrom <= 0) {
-            document.getElementById("cycleTimer").innerHTML = "";
             $("#cycleTimer").text("GOOD JOB!");
             $("#cycleTimer").css("font-size", "425%")
             $("#cycleTimer").css("color", "#4CAF50")
             $("#cycleTimer").toggleClass("blink");
         } else if (timerFrom < 10) {
-            document.getElementById("cycleTimer").innerHTML = "00" + timerFrom;
+            $("#cycleTimer").text("00" + timerFrom);
         } else if (timerFrom < 100) {
-            document.getElementById("cycleTimer").innerHTML = "0" + timerFrom;
+            $("#cycleTimer").text("0" + timerFrom);
         } else {
-            document.getElementById("cycleTimer").innerHTML = timerFrom;
+            $("#cycleTimer").text(timerFrom);
         }
     }
 }
